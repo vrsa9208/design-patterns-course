c97846d81a76c70648515249c52ca430df88bb31
vrsa9208 <vrsa9208@gmail.com>
2018-05-16 14:12:39 -0500

    Journal with single responsibilities

--- src/main/java/com/vrsa9208/unit1/solid/Journal.java
+++ src/main/java/com/vrsa9208/unit1/solid/Journal.java
@@ -1,5 +1,6 @@
 package com.vrsa9208.unit1.solid;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.net.URL;
@@ -10,36 +11,54 @@ public class Journal {
 
 	private final List<String> entries = new ArrayList<>();
 	private static int count = 0;
-	
+
 	public void addEntry(String text) {
 		entries.add("" + (++count) + ": " + text);
 	}
-	
+
 	public void removeEntry(int index) {
 		entries.remove(index);
 	}
-	
+
 	@Override
 	public String toString() {
 		return String.join(System.lineSeparator(), entries);
 	}
-	
-	public void save(String filename) throws FileNotFoundException {
-		try (PrintStream out = new PrintStream(filename)) {
-			out.println(toString());
+}
+
+class Persistence {
+	public void saveToFile(Journal journal, 
+			String filename, 
+			boolean overwrite) throws FileNotFoundException {
+		if (overwrite || new File(filename).exists()) {
+			try (PrintStream out = new PrintStream(filename)) {
+				out.println(journal.toString());
+			}
 		}
 	}
-	
-	public void load(String filename) { }
-	public void load(URL url) { }
+
+	public Journal load(String filename) {
+		return null;
+	}
+
+	public Journal load(URL url) {
+		return null;
+	}
+
 }
 
 class Demo {
-	
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		Journal j = new Journal();
 		j.addEntry("I cried today");
 		j.addEntry("I ate a bug");
 		System.out.println(j);
+		
+		Persistence p = new Persistence();
+		String filename = "C:\\temp\\journal.txt";
+		p.saveToFile(j, filename, true);
+		
+		Runtime.getRuntime().exec("notepad.exe " + filename);
 	}
 }
