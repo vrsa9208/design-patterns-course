a42e47a1e26cef037291cf307cf9974ddd6754e5
vvo0002 <vvo002@mx.wal-mart.com>
2018-05-17 12:08:53 -0500

    Interface segregation principle

--- src/main/java/com/vrsa9208/unit1/solid/InterfaceSegregationPrinciple.java
+++ src/main/java/com/vrsa9208/unit1/solid/InterfaceSegregationPrinciple.java
@@ -34,6 +34,58 @@ class MultiFunctionPrinter implements Machine {
 	
 }
 
+interface Printer {
+	void print(Document d);
+}
+
+interface Scanner {
+	void scan(Document d);
+}
+
+class JustAPrinter implements Printer {
+
+	@Override
+	public void print(Document d) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+class Photocopier implements Printer, Scanner {
+
+	@Override
+	public void scan(Document d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void print(Document d) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+interface MultiFunctionDevice extends Printer, Scanner { }
+
+class MultiFunctionMachine implements MultiFunctionDevice {
+
+	@Override
+	public void print(Document d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scan(Document d) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
 public class InterfaceSegregationPrinciple {
 
 }
